248de4d11d2da75d5a1182f2f350b91af2bf91f9
Russell Greene <russellgreene8@gmail.com>
2017-01-01 13:37:35 -0700

    Fix run.py

--- run.py
+++ run.py
@@ -9,24 +9,21 @@ if __name__ == "__main__":
         builder.add_common_builds()
     
     if platform.system() == "Linux":
-        for shared in [True, False]:
-            for build_type in ["Debug", "Release"]:
-                for gccver in builder.gcc_versions:
-                    builder.add({"arch": "x86_64",
-                                "build_type": build_type,
-                                "compiler": "gcc",
-                                "compiler.version": gccver,
-                                "compiler.libcxx": "libstdc++11"},
-                                {"QtBase:shared": shared})
+        for build_type in ["Debug", "Release"]:
+            for gccver in builder.gcc_versions:
+                builder.add({"arch": "x86_64",
+                            "build_type": build_type,
+                            "compiler": "gcc",
+                            "compiler.version": gccver,
+                            "compiler.libcxx": "libstdc++11"})
 
     if platform.system() == "Darwin":
-        for shared in [True, False]:
-            for compiler_version in builder.apple_clang_versions:
-                for build_type in ["Debug", "Release"]:
-                    builder.add({"arch": "x86_64",
-                                 "build_type": build_type,
-                                 "compiler": "apple-clang",
-                                 "compiler.version": compiler_version,
-                                 "llvm:shared": shared})
+        for compiler_version in builder.apple_clang_versions:
+            for build_type in ["Debug", "Release"]:
+                builder.add({"arch": "x86_64",
+                                "build_type": build_type,
+                                "compiler": "apple-clang",
+                                "compiler.version": compiler_version},
+                                {"QtSvg:shared": shared})
 
     builder.run()
